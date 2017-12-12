0ca7303af35a13eecadcd27d51f5b065d03c00bf
Olle Mansson <olle@brkt.com>
2017-12-11 19:27:22 -0800

    Update encryption script with the right port

--- tasks/encrypt-ami/task.sh
+++ tasks/encrypt-ami/task.sh
@@ -6,8 +6,9 @@ ami=$(cat stock_ami/ami)
 echo "Installing brkt-cli"
 pip install brkt-cli 1>/dev/null
 
-echo "Getting token from https://$SERVICE_DOMAIN"
-auth_cmd="brkt auth --email $EMAIL --password $PASSWORD --root-url https://$SERVICE_DOMAIN"
+echo "Getting token from https://$SERVICE_DOMAIN:443"
+auth_cmd="brkt auth --email $EMAIL --password $PASSWORD --root-url https://$SERVICE_DOMAIN:443"
+echo "Running command: brkt auth --email $EMAIL --password ***** --root-url https://$SERVICE_DOMAIN:443"
 export BRKT_API_TOKEN=`$auth_cmd`
 
 # echo "${CA_CERT}" > ${ROOT}/ca.crt
@@ -15,6 +16,7 @@ export BRKT_API_TOKEN=`$auth_cmd`
 
 echo "Encrypting stemcell image $ami"
 cmd="brkt aws encrypt --service-domain $SERVICE_DOMAIN --region $REGION --metavisor-version ${METAVISOR_VERSION} --no-single-disk --brkt-tag 'app=pcf' --brkt-tag 'role=opsmanager' $ami"
+echo "Running command: $cmd"
 $cmd | tee encrypt.log &
 
 # Wait for all background tasks to complete
