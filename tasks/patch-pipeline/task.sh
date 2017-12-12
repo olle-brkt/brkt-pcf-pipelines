625b1a0cc620b7da0dcb3253372c7cec77065cd8
Olle Mansson <olle@brkt.com>
2017-12-11 16:49:52 -0800

    Clean up the pipeline with fly before saving it

--- tasks/patch-pipeline/task.sh
+++ tasks/patch-pipeline/task.sh
@@ -10,7 +10,17 @@ curl \
 
 chmod +x yaml-patch
 
+curl \
+  --silent \
+  --insecure \
+  --output fly \
+  "${ATC_EXTERNAL_URL}/api/v1/cli?arch=amd64&platform=linux"
+
+chmod +x fly
+
 # Do ops here:
 cat pcf-pipelines/install-pcf/aws/pipeline.yml | \
   ./yaml-patch -o brkt-pcf-pipelines/operations/remove-bootstrap-tf-state.yml \
-  > generated-pipeline/pipeline.yml
+  > messy_pipeline.yml
+
+./fly format-pipeline -c messy_pipeline.yml > generated-pipeline/pipeline.yml
