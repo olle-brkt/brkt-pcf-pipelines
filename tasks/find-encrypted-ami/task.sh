6dd27dc001bdd9aedc4b42bbf44d8f828dba3126
Olle Mansson <olle@brkt.com>
2017-12-13 10:03:20 -0800

    Reworked find-enc-ami script

--- tasks/find-encrypted-ami/task.sh
+++ tasks/find-encrypted-ami/task.sh
@@ -4,7 +4,7 @@ set -eu
 echo "Installing pyyaml"
 pip install pyyaml 1>/dev/null
 
-touch encrypted_ami
+touch ami/ami
 
 ami=$(grep $REGION pivnet-opsmgr/*.yml | cut -d' ' -f2)
 
@@ -24,22 +24,23 @@ print "Currently encrypted amis: (in the format: Metavisor_version: [{source_ami
 for mv in data.items():
     print "%s: %s" % mv
 
-pre_enc_ami = None
+enc_ami = None
 
 if mv_ver in data:
     for ami in data[mv_ver]:
         source_ami = ami.items()[0][0]
         encrypted_ami = ami.items()[0][1]
         if src_ami.items()[0][0] == stock_ami:
-            pre_enc_ami = src_ami.items()[0][1]
+            enc_ami = src_ami.items()[0][1]
 
 
-with open('pre_encrypted_ami', 'w') as f:
-    if pre_enc_ami is None:
-        print "Can not reuse pre-encrypted ami, not found"
+with open('ami/ami', 'w') as f:
+    if enc_ami is None:
+        print "Can not reuse pre-encrypted ami, not found! Start the encrypt job again."
         f.write("Not found")
+        exit(1)
     else:
-        print "%s has already been encrypted by %s. Resulting ami %s. Reusing it to save time" % (stock_ami, mv_ver, pre_enc_ami)
-        f.write(pre_enc_ami)
+        print "Found %s" % enc_ami)
+        f.write(enc_ami)
 EOF
 
