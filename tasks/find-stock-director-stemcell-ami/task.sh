485e45dcd10ca0d85755ab1432a734cdece60a29
Olle Mansson <olle@brkt.com>
2018-01-06 14:24:08 -0800

    first draft of director encryption

---
+++ tasks/find-stock-director-stemcell-ami/task.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu
+
+echo "$PEM" > ssh-key
+chmod 700 ssh-key
+
+echo "moving the stock stemcell.tgz from /var/tempest/stemcells to /tmp/stock_stemcells/..."
+ssh -i ssh-key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oLogLevel=error ubuntu@$OPSMAN_DOMAIN_OR_IP_ADDRESS 'mkdir -p /tmp/stock_stemcells/; mv /var/tempest/stemcells/*.tgz /tmp/stock_stemcells/'
+
+echo "scp:ing the stock stemcell from $OPSMAN_DOMAIN_OR_IP_ADDRESS"
+scp -i ssh-key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oLogLevel=error ubuntu@$OPSMAN_DOMAIN_OR_IP_ADDRESS:'/tmp/stock_stemcells/*.tgz' ./
+
+sc_file_path=$(find ./ -name *.tgz)
+
+if [ ! -f "$sc_file_path" ]; then
+  echo "Stemcell file not found!"
+  exit 1
+fi
+
+# stock stemcell to stemcell/$sc_file_path
+cp $sc_file_path stemcell/$sc_file_path
+
+tar xf $sc_file_path
+source_ami=$(grep "$REGION" stemcell.MF | awk '{print $2}')
+echo "Stock stemcell ami: $source_ami"
+
+# stock stemcell ami to stock-ami/ami
+echo "$source_ami" > stock-ami/ami
---
+++ tasks/upload-director-stemcell/task.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+echo "$PEM" > ssh-key
+chmod 700 ssh-key
+
+mv encrypted-stemcell/*.tgz ./
+
+sc_file_path=$(find ./ -name *.tgz)
+
+if [ ! -f "$sc_file_path" ]; then
+  echo "Stemcell file not found!"
+  exit 1
+fi
+
+ssh -i ssh-key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oLogLevel=error ubuntu@$OPSMAN_DOMAIN_OR_IP_ADDRESS 'mkdir -p /tmp/encrypted_stemcells/'
+echo "scp:ing the encrypted stemcell to $OPSMAN_DOMAIN_OR_IP_ADDRESS"
+scp -i ssh-key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oLogLevel=error $sc_file_path ubuntu@$OPSMAN_DOMAIN_OR_IP_ADDRESS:/tmp/encrypted_stemcells/
+
+echo "moving the stock stemcell.tgz from /var/tempest/stemcells to /tmp/encrypted_stemcells/..."
+ssh -i ssh-key -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oLogLevel=error ubuntu@$OPSMAN_DOMAIN_OR_IP_ADDRESS 'mv /tmp/encrypted_stemcells/*.tgz /var/tempest/stemcells/'
