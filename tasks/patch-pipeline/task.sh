9d639bfd4b118a7c2e7657f9b18575ad58a034d1
Olle Mansson <olle@brkt.com>
2018-01-23 11:59:19 -0800

    updated debug print

--- tasks/patch-pipeline/task.sh
+++ tasks/patch-pipeline/task.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -eu
 
-echo "patching pipeline..."
+echo "getting fly-cli..."
 
 curl -L -s -k -o fly "${ATC_EXTERNAL_URL}/api/v1/cli?arch=amd64&platform=linux"
 chmod +x fly
 
+echo "patching pipeline..."
+
 cat pcf-pipelines/install-pcf/aws/pipeline.yml | yaml-patch \
     -o brkt-pcf-pipelines/operations/brktize-pipeline.yml \
     > messy_pipeline.yml
+
+echo "formatting pipeline..."
 
 ./fly format-pipeline -c messy_pipeline.yml > generated-pipeline/pipeline.yml
