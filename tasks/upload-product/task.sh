14ab97e37269b5655c91431cd2fc89dea23b902b
Olle Mansson <olle@brkt.com>
2018-02-01 15:34:59 -0800

    removed unneeded params

--- tasks/upload-product/task.sh
+++ tasks/upload-product/task.sh
@@ -6,8 +6,6 @@ echo "uploading product"
 # Should the slug contain more than one product, pick only the first.
 file_path=$(find ./pivnet-product -name *.pivotal | sort | head -1)
 om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-  --client-id "${OPSMAN_CLIENT_ID}" \
-  --client-secret "${OPSMAN_CLIENT_SECRET}" \
   -u "$OPSMAN_USR" \
   -p "$OPSMAN_PWD" \
   -k \
